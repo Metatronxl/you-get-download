3fd3f221f4b13c86b150f4150171567acee32361
Metatronxl <8b:b6:7d:29:b0:c3:da:1d:d3:3d:58:6f:d7:76:32:9e>
2018-09-20 11:09:28 +0800

    添加多进程支持

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 import requests
 from configparser import RawConfigParser
 import os
+from multiprocessing import Process
 
 class MainDownload:
     def __init__(self):
@@ -20,20 +21,37 @@ class MainDownload:
             info = os.system(r'you-get --debug -o /Users/xulei2/Downloads  {}'.format(url))
             print(info)
         except Exception as e:
-            print(e)
+            # print(e)
+            print("error exist ,re-download")
             self.cmd_download(url)
+
+    def multi_download(self,download_url_list):
+        processes = list()
+        for singlen_url in download_url_list:
+            p = Process(target=self.cmd_download,args=(singlen_url,))
+            print("开始一个新的进程任务")
+            p.start()
+            processes.append(p)
+        for p in processes:
+            p.join()
+        print("多线程任务结束")
+
+    def sequence_download(self,download_url_list):
+        for single_url in download_url_list:
+            self.cmd_download(single_url)
+
+
 
     def generate_download_url(self):
 
         download_url_list = [ self.mainPage+str(i) for i in range(self.startId,self.startId+self.num)]
-        for singlen_url in download_url_list:
-            self.cmd_download(singlen_url)
+        return download_url_list
 
 
 
 if __name__ == '__main__':
 
     mainDownload = MainDownload()
-    mainDownload.generate_download_url()
-
+    url_list = mainDownload.generate_download_url()
+    mainDownload.multi_download(url_list)
 
